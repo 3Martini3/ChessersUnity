315f7cae22cbf1354697db0d83ffbe05881d1f72
AbyssvvaIker <Krzychu@Chessers>
2021-03-30 17:09:21 +0200

    testing
    tests

---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//just testing something
+
+public class TEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
