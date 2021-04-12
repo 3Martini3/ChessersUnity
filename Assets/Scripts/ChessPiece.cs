980982b10dfda08a316eea7a04ffa803c4348ab9
3Martini3 <martini@chessers>
2021-04-12 21:51:18 +0200

    ChessPiece hover created

--- Assets/Scripts/ChessPiece.cs
+++ Assets/Scripts/ChessPiece.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class ChessPiece : MonoBehaviour {
 
     public MeshRenderer square;
-    private float yPosition;
+    public Material hoverMaterial;
+    private float yPosition { get; set; }
+    private Material originalMaterial { get; set; }
+    private Color defaultColor { get; set; }
     // Start is called before the first frame update
     void Start()
     {
         yPosition = transform.position.y;
         transform.position = new Vector3(square.bounds.center.x, yPosition, square.bounds.center.z);
+        originalMaterial = GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
-   /* void Update()
+
+    private void OnMouseOver()
     {
-        
-    }*/
+        GetComponent<Renderer>().material = hoverMaterial;
+    }
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = originalMaterial;
+    }
+    // Update is called once per frame
+    /* void Update()
+     {
+
+     }*/
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
         
+
+        }
     }
 }
