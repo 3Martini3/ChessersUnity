ddda65deef6f5978abc5ec10ad7f118752af7c5f
3Martini3 <martini@chessers>
2021-04-13 01:01:16 +0200

    ChessPieces movement, drag and drop added (not assigned to squares, no chess logic)

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -2,32 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-class DragTransform : MonoBehaviour
+class DragAndDrop : MonoBehaviour
 {
 
     private bool dragging = false;
-    private float distance;
+    private float distance { get; set; }
+    private float defaultYPosition { get; set; }
 
-
+    void Start()
+    {
+        defaultYPosition=transform.position.y;
+    }
 
     void OnMouseDown()
     {
         distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
         dragging = true;
     }
 
     void OnMouseUp()
     {
         dragging = false;
+        transform.position = new Vector3(transform.position.x, defaultYPosition, transform.position.z);
     }
 
     void Update()
     {
         if (dragging)
         {
+            distance = Vector3.Distance(new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z), Camera.main.transform.position);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Vector3 rayPoint = ray.GetPoint(distance);
-            transform.position = rayPoint;
+            transform.position = new Vector3(rayPoint.x, transform.position.y, rayPoint.z);
         }
     }
 }
