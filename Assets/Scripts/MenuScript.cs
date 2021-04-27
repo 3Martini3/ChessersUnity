31062b82db5f88c34ab8d2a4af01f5cf23b1d426
shane3102 <bkucharczyk2000@chessers>
2021-04-27 14:26:56 +0200

    Dodano funkcjonalność do menu ustawień
    Dodano kontrole muzyki, suwak efektów dźwiękowych (nieaktywny), rozwijaną listę rozdzielczości, rozwijaną listę jakości tekstur, opcje przełączenia na pełny ekran

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,19 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            //szukam rozdzielczoœci równej rozdzielczoœci monitora u¿ytkownika
+            if(Screen.currentResolution.width == resolutions[i].width && Screen.currentResolution.height == resolutions[i].height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+
+        //ustalam wrtoœæ na liœcie na równ¹ rozdzielczoœci monitora u¿ytkownika
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        //ustawiam gre nia widok nie-pe³noekranowy
+        Screen.fullScreen = false;
+    }
+
     public void PlayChess()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("Scenes/MartaTesty");
     }
+
     public void PlayCheckers()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Scenes/Main");
     }
+
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetQuality(int level)
+    {
+        // 0 - high
+        // 1 - medium
+        // 2 - low
+        QualitySettings.SetQualityLevel(2 - level);
+    }
+
+    public void SetFullscreen(bool toogleState)
+    {
+        Screen.fullScreen = toogleState;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution chosenResolution = resolutions[resolutionIndex];
+        Screen.SetResolution(chosenResolution.width, chosenResolution.height,Screen.fullScreen);
     }
 }
