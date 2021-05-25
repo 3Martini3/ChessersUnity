3a0c9540c1c2e12dc03c2ff1d6f81e76a4a9d155
Dziadziumil <Dziadziumil@chessers>
2021-05-25 21:29:35 +0200

    Added  prototype countdown clock on canvas

---
+++ Assets/Scripts/TimerCanvas.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TimerCanvas : MonoBehaviour
+{
+    public float timeRemaining = 3600;
+    public bool timerIsRunning = false;
+    public string timeText;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        GameObject.FindWithTag("Canvas").GetComponent<Text>().text = timeText;
+    }
+}
