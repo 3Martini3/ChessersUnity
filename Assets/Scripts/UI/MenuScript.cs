3497adb521240df25b3fc7fd65a7ba778afd7c62
Dziadziumil <Dziadziumil@chessers>
2021-06-20 15:28:56 +0200

    Added documentary comments for scripts from ui folder

--- Assets/Scripts/UI/MenuScript.cs
+++ Assets/Scripts/UI/MenuScript.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Loads scenne with chess/ quit aplication
+/// </summary>
 public class MenuScript : MonoBehaviour
 {
-
+    /// <summary>
+    /// Load chess
+    /// </summary>
     public void PlayChess()
     {
         SceneManager.LoadScene("Scenes/ChessScene/GameScene");
     }
-
-    public void PlayCheckers()
-    {
-        SceneManager.LoadScene("Scenes/CheckersScene/GameSceneCheckers");
-    }
-
+    /// <summary>
+    /// quits aplication
+    /// </summary>
     public void Quit()
     {
         Application.Quit();
