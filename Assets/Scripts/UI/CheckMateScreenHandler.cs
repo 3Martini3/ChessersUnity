a744b469116467452d1f9f4cc22ac696338194f6
AbyssvvaIker <Krzychu@Chessers>
2021-06-20 14:03:21 +0200

    Added ending screens when timers runs out (separately for black and white)

--- Assets/Scripts/UI/CheckMateScreenHandler.cs
+++ Assets/Scripts/UI/CheckMateScreenHandler.cs
@@ -7,6 +7,11 @@ public class CheckMateScreenHandler : MonoBehaviour
 {
     public GameObject checkMateCanvas;
 
+
+    /// <summary>
+    /// shows end screen, depending on parameter- type of information, 1 white won, 2 black won, 3 a tie
+    /// </summary>
+    /// <param name="checkMateType"></param>
     public void ShowEndScreen(int checkMateType)
     {
         if(checkMateType == 1)
