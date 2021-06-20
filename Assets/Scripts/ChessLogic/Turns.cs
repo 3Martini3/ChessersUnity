9e32622832315a3bfcafdecff67de366d111e34b
Dziadziumil <Dziadziumil@chessers>
2021-06-20 14:39:32 +0200

    Finished chess logic folder comments

--- Assets/Scripts/ChessLogic/Turns.cs
+++ Assets/Scripts/ChessLogic/Turns.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+/// <summary>
+/// Swap turn and send text infromation about it to game
+/// </summary>
 public class Turns : MonoBehaviour
 {
     public ChessEnum.Color turn;
 
+    /// <summary>
+    /// Swaps turn to the next player (color) and sendt text info
+    /// </summary>
     public void SwapTurn()
     {
         if (turn == ChessEnum.Color.White)
